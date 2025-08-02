328a72526f2899df8bce1a1c6bf76a0eed28c4a1
Aleksey <kot543212@mail.ru>
2025-08-02 14:11:43 +0300

    HomeWork complete

--- src/main/java/ru/otus/java/basic/june/http/server/HttpServer.java
+++ src/main/java/ru/otus/java/basic/june/http/server/HttpServer.java
@@ -1,12 +1,14 @@
 package ru.otus.java.basic.june.http.server;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpServer {
-    private int port;
-    private Dispatcher dispatcher;
+    private final int port;
+    private final Dispatcher dispatcher;
 
     public HttpServer(int port) {
         this.port = port;
@@ -17,20 +19,41 @@ public class HttpServer {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту " + port + ". Ожидаем подключения");
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    byte[] buffer = new byte[8192];
-                    int n = socket.getInputStream().read(buffer);
-                    if (n < 1) {
-                        continue;
-                    }
-                    String rawRequest = new String(buffer, 0, n);
-                    HttpRequest request = new HttpRequest(rawRequest);
-                    request.info(true);
-                    dispatcher.execute(request, socket.getOutputStream());
-                }
+                Socket socket = serverSocket.accept();
+                Thread thread = new Thread(new Threads(socket, dispatcher));
+                thread.start();
+
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static class Threads implements Runnable {
+        private final Socket socket;
+        private final Dispatcher dispatcher;
+
+        public Threads(Socket socket, Dispatcher dispatcher) {
+            this.socket = socket;
+            this.dispatcher = dispatcher;
+        }
+
+        @Override
+        public void run() {
+            try (InputStream inputStream = socket.getInputStream();
+                 OutputStream outputStream = socket.getOutputStream()) {
+                byte[] buffer = new byte[8192];
+                int bytesRead = inputStream.read(buffer);
+                if (bytesRead < 1) {
+                    return;
+                }
+                String rawRequest = new String(buffer, 0, bytesRead);
+                HttpRequest request = new HttpRequest(rawRequest);
+                request.info(true);
+                dispatcher.execute(request, outputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
